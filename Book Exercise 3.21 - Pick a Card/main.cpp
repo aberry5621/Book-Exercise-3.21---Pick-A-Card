e6e2cd33386b7b7b3c47b97375a80db4317d8d7b
alexberry721 <alexberry721@gmail.com>
2016-09-01 09:13:59 -0700

    Initial Commit

---
+++ Book Exercise 3.21 - Pick a Card/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Book Exercise 3.21 - Pick a Card
+//
+//  Created by ax on 9/1/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
